502e5cc2309b52d896e261c5701d189548aaedd2
dvub <dvubdevs@gmail.com>
2024-12-17 23:05:24 -0800

    Add files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Circle Of 5ths
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Circle Of 5ths as follows:
+
+```shell
+cargo xtask bundle circle_of_5ths --release
+```
---
+++ src/editor.rs
@@ -0,0 +1,64 @@
+use std::sync::{Arc, Mutex};
+
+use nih_plug::midi::{MidiResult, NoteEvent};
+use nih_plug_webview::{
+    DropData, DropEffect, EventStatus, HTMLSource, Key, MouseEvent, WebViewEditor,
+};
+use rtrb::Consumer;
+use serde_json::json;
+
+pub fn create_editor(buffer: Consumer<NoteEvent<()>>) -> WebViewEditor {
+    let output = Arc::new(Mutex::new(buffer));
+
+    let size = (400, 400);
+
+    #[cfg(debug_assertions)]
+    let src = HTMLSource::URL("http://localhost:3000".to_owned());
+    #[cfg(debug_assertions)]
+    let mut editor = WebViewEditor::new(src, size);
+
+    #[cfg(not(debug_assertions))]
+    let mut editor = todo!();
+
+    editor = editor
+        .with_developer_mode(true)
+        .with_keyboard_handler(move |event| {
+            println!("keyboard event: {event:#?}");
+            event.key == Key::Escape
+        })
+        .with_mouse_handler(|event| match event {
+            MouseEvent::DragEntered { .. } => {
+                println!("drag entered");
+                EventStatus::AcceptDrop(DropEffect::Copy)
+            }
+            MouseEvent::DragMoved { .. } => {
+                println!("drag moved");
+                EventStatus::AcceptDrop(DropEffect::Copy)
+            }
+            MouseEvent::DragLeft => {
+                println!("drag left");
+                EventStatus::Ignored
+            }
+            MouseEvent::DragDropped { data, .. } => {
+                if let DropData::Files(files) = data {
+                    println!("drag dropped: {:?}", files);
+                }
+                EventStatus::AcceptDrop(DropEffect::Copy)
+            }
+            _ => EventStatus::Ignored,
+        })
+        .with_event_loop(move |ctx, _setter, _window| {
+            while let Ok(_value) = ctx.next_event() {}
+
+            // TODO:
+            // does unwrap need to be fixed?
+            let mut output_buffer_lock = output.lock().unwrap();
+
+            if let Ok(raw_midi_data) = output_buffer_lock.pop() {
+                if let Some(MidiResult::Basic(midi_data_array)) = raw_midi_data.as_midi() {
+                    ctx.send_json(json!(midi_data_array)).unwrap();
+                }
+            }
+        });
+    editor
+}
